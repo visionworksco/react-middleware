e27c86492fd45e9b1a0a03bfa5c287120ecb74f6
spetushkov <sergey.petushkov@gmail.com>
2022-01-02 14:46:19 +0100

    feat: :rocket:

--- jest.config.js
+++ jest.config.js
@@ -3,11 +3,6 @@ module.exports = {
   transform: { '^.+\\.(ts|tsx|js|jsx)?$': 'ts-jest' },
   transformIgnorePatterns: ['/node_modules/(?!antd|@ant-design|rc-.+?|@babel/runtime).+(js|jsx)$'],
   testRegex: '(/__test__/.*|(\\.|/)(test|spec))\\.[jt]sx?$',
-  moduleNameMapper: {
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/__mocks__/fileMock.js',
-    '\\.(css|scss)$': 'identity-obj-proxy',
-  },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   modulePathIgnorePatterns: [
     '<rootDir>/src/__test__/jest.setup.js',
